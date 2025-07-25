47104dc09e8aa115742472442520b32d2f3aa443
pesuts <daniel.sinaga.ds@gmail.com>
2025-07-25 08:58:19 +0700

    chore: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import router from "./routes";
+const cors = require("cors");
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -7,6 +8,7 @@ dotenv.config();
 const app = express();
 const PORT: number = parseInt(process.env.PORT || "8080", 10);
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
