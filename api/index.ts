1a7b5410ef54cdf5e144fec4d66710e00851280d
pesuts <daniel.sinaga.ds@gmail.com>
2025-07-25 10:17:00 +0700

    chore: configure to deploy in vercel

---
+++ api/index.ts
@@ -0,0 +1,9 @@
+import app from '../src';
+
+export default app;
+// import { createServer } from 'http';
+// const server = createServer(app);
+
+// export default async function handler(req: any, res: any) {
+//   server.emit('request', req, res);
+// }
