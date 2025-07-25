1ed1a55cb19647dd1678ef287db856a0fc992976
pesuts <daniel.sinaga.ds@gmail.com>
2025-07-25 10:32:05 +0700

    chore: configure to deploy in vercel

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,26 @@
-import app from '../src';
+import express from "express";
+import router from "../src/routes";
+const cors = require("cors");
 
-export default app;
-// import { createServer } from 'http';
-// const server = createServer(app);
+import dotenv from "dotenv";
+dotenv.config();
 
-// export default async function handler(req: any, res: any) {
-//   server.emit('request', req, res);
-// }
+import { createServer } from "http";
+
+const app = express();
+const PORT: number = parseInt(process.env.PORT || "8080", 10);
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+// Wrap with Vercel-compatible handler
+const server = createServer(app);
+
+export default function handler(req: any, res: any) {
+  return server.emit("request", req, res);
+}
+// export default app;
+// app.listen(PORT, () => {
+//   console.log(`Server is running at http://localhost:${PORT}`);
+// });
